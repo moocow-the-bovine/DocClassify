58ff71252a89d6e902e0a4685a8719b0e96a4019
Bryan Jurish <jurish@bbaw.de>
2009-11-03 15:45:12 +0000

    git-svn-id: svn+ssh://svn.dwds.de/home/moocow/svn/public/classify/trunk@2604 097f6656-c427-11dc-a790-1b5d5187385a

--- zzz-xcheck-many.sh
+++ zzz-xcheck-many.sh
@@ -1,13 +1,30 @@
 #!/bin/sh
 
 #base=vzdata-safe
-#base=vzdata-all
+base=vzdata-all
 ##--
-base=vzdata-safe.head
+#base=vzdata-safe.head
+#base=vzdata-all.head1p
 
-for mdf in 1 2; do
- for mf in 1 2 4; do
-  ./dc-mapper-xcheck.perl -n=10 -seed=0 -r=128 -mf=$mf -mdf=$mdf -od ${base}.n-10.seed-0.r-128.mf-${mf}.mdf-${mdf}.xcheck.d ${base}.corpus.xml
+##-- test config(s)
+mdfs="1"
+##--
+mfs="1"
+#mfs="1 2 4"
+#mfs="1 2 4 8 16 24"
+#mfs="8 16 24"
+#mfs="8"
+##--
+#dists="u"
+dists="c"
+##--
+
+for dist in $dists; do
+ for mdf in $mdfs; do
+  for mf in $mfs; do
+   echo "$0[$$]: base=$base; mf=$mf; mdf=$mdf; dist=$dist"
+   ./dc-mapper-xcheck.perl -n=10 -seed=0 -r=128 -mf=$mf -mdf=$mdf -od ${base}.n-10.seed-0.r-128.mf-${mf}.mdf-${mdf}.dist-${dist}.xcheck.d ${base}.corpus.xml
+  done
  done
 done
-
+echo "$0[$$]: done.\n";
