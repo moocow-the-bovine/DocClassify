9c2de9d97f222ebda8acc55da6089a97c72d843b
Bryan Jurish <jurish@bbaw.de>
2009-12-16 15:12:11 +0000

    git-svn-id: svn+ssh://svn.dwds.de/home/moocow/svn/public/classify/trunk@2667 097f6656-c427-11dc-a790-1b5d5187385a

---
+++ setbase.sh
@@ -0,0 +1,1 @@
+base=data/train_data_2009_12_16_v1; tbase=data/test_data_2009_12_16_v1; mc=LSI; mc_wcat=1; lzc=VzSem; lz_semw=10; r=512; tw=Hmax; mbase=$base.mc-$mc.wcat-$mc_wcat.lzc-$lzc.semw-$lz_semw.r-$r.tw-$tw;
