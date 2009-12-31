ca4f03208e2e02559562331b5da7f68af4adc0e7
Bryan Jurish <jurish@bbaw.de>
2009-12-31 11:43:15 +0000

    git-svn-id: svn+ssh://svn.dwds.de/home/moocow/svn/public/classify/trunk@2679 097f6656-c427-11dc-a790-1b5d5187385a

---
+++ train+apply+eval.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+#DUMMY='yes'
+
+if test $# -lt 2; then
+  echo "Usage: $0 TRAIN_BASE TEST_BASE [INFIX='' [TRAINARGS=none]]"
+  exit 1;
+fi
+
+train="$1"
+shift;
+test="$1"
+shift;
+infix="$1"
+shift;
+
+rc=0
+
+if test -f ./dc-mapper-train.perl ; then
+  progdir=./
+elif test -f ../dc-mapper-train.perl ; then
+  progdir=../
+else
+  progdir=""
+fi
+
+runcmd() {
+  echo "$@"
+  test -n "$DUMMY" || "$@" || rc=1
+}
+
+echo "##=================================================================="
+echo "## $0: TRAIN"
+runcmd ${progdir}dc-mapper-train.perl "$train.corpus.xml" "$@" -o "$train.$infix.bin" || exit 1
+
+echo ""
+echo "##=================================================================="
+echo "## $0: APPLY"
+runcmd ${progdir}dc-mapper-apply.perl "$train.$infix.bin" "$test.corpus.xml" -o "$test.$infix.out.xml" || exit 2
+
+echo ""
+echo "##=================================================================="
+echo "## $0: EVAL"
+runcmd ${progdir}dc-mapper-eval.perl "$test.corpus.xml" "$test.$infix.out.xml" -o "$test.$infix.eval.xml" || exit 3
+
+exit 0
