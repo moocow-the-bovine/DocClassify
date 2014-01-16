37a27e424b457ab3a8f3bb1acb6d69f4f45bbde9
Bryan Jurish <jurish@bbaw.de>
2014-01-16 15:16:12 +0000

    cab-regen fixes
    git-svn-id: svn+ssh://svn.dwds.de/home/moocow/svn/public/classify/trunk@2748 097f6656-c427-11dc-a790-1b5d5187385a

--- cab-regen.sh
+++ cab-regen.sh
@@ -2,10 +2,10 @@
 
 src="$1"
 base="$2"
-test -n "$src"  || src=data/cab-ner
+test -n "$src"  || src=data/dta-ner
 test -n "$base" || base="${src%.d}"
 
-dummy=""
+#dummy=""
 
 runcmd() {
   echo "$@" >&2
@@ -16,8 +16,13 @@ runcmd() {
 runcmd ./dc-corpus-create.perl -v=3 -dc=cab1g -o=$base.corpus.xml $src
 
 ##-- train
-runcmd ./dc-mapper-train.perl -r=64 -lzc=Cab -mdf=2 -mf=5 -nonull -mo=weightByCat=0 -noclear -v=3 -o=$base.map.bin $base.corpus.xml
+for wraw in 0 1 ; do
+ echo "=== twRaw=$wraw"
+ map="$base.wraw${wraw}.map.bin"
+ runcmd ./dc-mapper-train.perl -r=64 -lzc=Cab -mdf=1 -mf=2 -nonull -mo=twRaw=$wraw -mo=weightByCat=0 -noclear -v=3 -o=$map $base.corpus.xml
 
-##-- test
-runcmd ./testme.perl $base.map.bin -raw   > $base.prof-raw
-runcmd ./testme.perl $base.map.bin -noraw > $base.prof-cooked
+ ##-- test
+ #runcmd ./testme.perl $map -raw    > $base.prof-raw
+ #runcmd ./testme.perl $map -cooked > $base.prof-cooked
+ runcmd ./testme.perl $map -json > $base.json
+done
