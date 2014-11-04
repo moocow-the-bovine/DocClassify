04c5f657557f1f7aea6acad058c1d7ceab9f5b6b
Bryan Jurish <jurish@bbaw.de>
2014-11-04 08:43:08 +0000

    + updated to v0.10
    git-svn-id: svn+ssh://svn.dwds.de/home/moocow/svn/public/classify/trunk@2773 097f6656-c427-11dc-a790-1b5d5187385a

--- DocClassify.pm
+++ DocClassify.pm
@@ -41,7 +41,7 @@ use strict;
 ##==============================================================================
 ## Globals & Constants
 
-our $VERSION = 0.09;
+our $VERSION = 0.10;
 our @ISA = ('DocClassify::Logger'); ##-- for convenience
 
 BEGIN {
