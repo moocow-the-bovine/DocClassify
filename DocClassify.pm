1d9a378c6e840a8a96f93e715f9ecaca10861174
Bryan Jurish <jurish@bbaw.de>
2015-03-24 12:15:45 +0000

    + v0.12: directory I/O via mmap seems to be working
    git-svn-id: svn+ssh://svn.dwds.de/home/moocow/svn/public/classify/trunk@2787 097f6656-c427-11dc-a790-1b5d5187385a

--- DocClassify.pm
+++ DocClassify.pm
@@ -41,7 +41,7 @@ use strict;
 ##==============================================================================
 ## Globals & Constants
 
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 our @ISA = ('DocClassify::Logger'); ##-- for convenience
 
 BEGIN {
