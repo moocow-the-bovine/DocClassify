1cdb969391b938d5fdbde374d0d29139a6b268d1
Bryan Jurish <jurish@bbaw.de>
2015-03-25 11:41:56 +0000

    + DocClassify::Logger: added 'logdate', 'logtime' options
    git-svn-id: svn+ssh://svn.dwds.de/home/moocow/svn/public/classify/trunk@2795 097f6656-c427-11dc-a790-1b5d5187385a

--- DocClassify/Logger.pm
+++ DocClassify/Logger.pm
@@ -28,6 +28,8 @@ BEGIN {
        rootLevel => ($^W ? 'WARN' : 'FATAL'),
        level     => ($^W ? $MIN_LEVEL : 'INFO'),
        stderr    => 1,
+       logdate   => 1,     ##-- log date+time?
+       logtime   => 1,     ##-- log time only?
        file      => undef,
        rotate    => undef, ##-- default: haveFileRotate()
        syslog    => 0,
@@ -45,6 +47,8 @@ BEGIN {
 ##     rootLevel => $LEVEL_OR_UNDEF,  ##-- min root log level (default='WARN' or 'FATAL', depending on $^W)
 ##     level     => $LEVEL_OR_UNDEF,  ##-- min log level (default=$MIN_LEVEL or 'INFO', depending on $^W)
 ##     stderr    => $bool,            ##-- whether to log to stderr (default=1)
+##     logdate   => $bool,            ##-- whether to log date+time-stamps on stderr (default=0)
+##     logtime   => $bool,            ##-- whether to log time-stamps on stderr (default=0)
 ##     file      => $filename,        ##-- log to $filename if true
 ##     rotate    => $bool,            ##-- use Log::Dispatch::FileRotate if available and $filename is true
 ##     syslog    => $bool,            ##-- use Log::Dispatch::Syslog if available and true (default=false)
@@ -88,12 +92,14 @@ log4perl.PatternLayout.cspec.G = sub { return File::Basename::basename(\"$::0\")
 ";
 
   ##-- appender: stderr
+  my $stderr_date = ($opts{logdate} ? '%d{yyyy-MM-dd HH:mm:ss} '
+		     : ($opts{logtime} ? '%d{HH:mm:ss} ' : ''));
   $cfg .= "
 ##-- Appender: AppStderr
 log4perl.appender.AppStderr = Log::Log4perl::Appender::Screen
 log4perl.appender.AppStderr.stderr = 1
 log4perl.appender.AppStderr.layout = Log::Log4perl::Layout::PatternLayout
-log4perl.appender.AppStderr.layout.ConversionPattern = %G[%P] %p: %c: %m%n
+log4perl.appender.AppStderr.layout.ConversionPattern = ${stderr_date}%G[%P] %p: %c: %m%n
 ";
 
   ##-- appender: syslog
